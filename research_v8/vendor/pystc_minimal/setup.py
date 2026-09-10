d08adaaeea1cbd4aeee089ec9803c022cc5eca6a
EkodeckStephane <166718028+EkodeckStephane@users.noreply.github.com>
2026-09-10 21:32:39 +0100

    Add STC extension build script

---
+++ research_v8/vendor/pystc_minimal/setup.py
@@ -0,0 +1,15 @@
+import os, sys
+from setuptools import setup, Extension, find_packages
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c++11"]
+    extra_link_args = []
+elif sys.platform == "darwin":
+    extra_compile_args = ["-std=c++11", "-stdlib=libc++"]
+    extra_link_args = []
+else:
+    extra_compile_args = ["-std=c++11", "-Wno-narrowing"]
+    extra_link_args = []
+stc_extension = Extension('pystc.stc_extension', include_dirs=['pystc/'],
+    sources=['pystc/common.cpp','pystc/stc_embed_c.cpp','pystc/stc_extract_c.cpp','pystc/stc_interface.cpp','pystc/stc_ml_c.cpp'],
+    extra_compile_args=extra_compile_args, extra_link_args=extra_link_args, language='c++')
+setup(name='pystcstego', version='0.1.0', packages=find_packages(), ext_modules=[stc_extension])
